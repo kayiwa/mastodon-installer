e66b93d2f26c856095df5690e23e744c26f75306
Francis Kayiwa <kayiwa@pobox.com>
2019-11-12 11:26:45 -0500

    add tests

--- roles/system_packages/molecule/default/tests/test_default.py
+++ roles/system_packages/molecule/default/tests/test_default.py
@@ -1,10 +1,29 @@
 import os
+import pytest
 
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']
 ).get_hosts('all')
+
+
+@pytest.mark.parametrize("name", [
+    "imagemagick",
+    "python-apt",
+    "ffmpeg",
+    "libxml2-dev",
+    "libxslt1-dev",
+    "file",
+    "git",
+    "autoconf",
+    "protobuf-compiler",
+    "g++"
+])
+def test_for_system_packages(host, name):
+    pkg = host.package(name)
+
+    assert pkg.is_installed
 
 
 def test_hosts_file(host):
